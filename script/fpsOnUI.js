f68b226686763a94e791a6bf5d413e2afe4962dd
LombreBlanche34 <lombreblanche34@gmail.com>
2022-10-07 13:48:34 +0200

    fix FPS UI on first start

--- script/fpsOnUI.js
+++ script/fpsOnUI.js
@@ -3,14 +3,16 @@ const config = new Store();
 
 let fpsOnUI = function () {
     let menuBox = document.querySelector("#app > app-interface > div:nth-child(3) > menu-interface > menu-tools")
-    let FPS = document.querySelector("#app > app-interface > div:nth-child(2) > game-interface > game-stats > stat-item:nth-child(1) > span:nth-child(1)");
+    var FPS = document.querySelector("#app > app-interface > div:nth-child(2) > game-interface > game-stats > stat-item:nth-child(1) > span:nth-child(1)");
     menuBox.insertAdjacentHTML("afterbegin", `
     <span id="FPSUI" style="font-size: 2vw;"></span>
     `)
 
     if (!config.get("fpsOnUI")) {
         console.log("[SCRIPT] fpsOnUI: " + false)
-        return false;
+        document.getElementById("FPSUI").style.display = "none";
+    } else {
+        document.getElementById("FPSUI").style.display = "block";
     }
 
     console.log("[SCRIPT] fpsOnUI: " + true);
